991635d77ff688f65bbe42c705123ca38ba55771
Max Janorschke <programming@max-janorschke.de>
2023-03-21 01:22:39 +0100

    Basic Setup #1
    * setup i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from 'context/ThemeContext/ThemeContext';
 import { CookiesProvider } from 'react-cookie';
 import { Routes } from 'routes/Routes';
+import { TranslationProvider } from 'context/TranslationContext/TranslationContext';
 
 export const App = () => {
   return (
     <CookiesProvider>
       <ThemeProvider>
-        <Routes />
+        <TranslationProvider>
+          <Routes />
+        </TranslationProvider>
       </ThemeProvider>
     </CookiesProvider>
   );
