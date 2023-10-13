581185506e03881f27396451fe09aa83c1b978c8
Max Janorschke <programming@max-janorschke.de>
2023-10-13 19:04:46 +0200

    refactor: :fire: Replace custom locales by i18next

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
+// START: DONT REMOVE THE FOLLOWING IMPORTS
+import './i18n';
+// END: DONT REMOVE THE FOLLOWING IMPORTS
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
--- src/components/ErrorFallbackPage.tsx
+++ src/components/ErrorFallbackPage.tsx
@@ -1,39 +1,38 @@
-import { useLanguageContext } from '@context/LanguageContext';
 import { Button, Card, CardContent, CardHeader, CardMedia, Link, Stack, Typography } from '@mui/material';
 import { paths } from '@routes';
 import { FC } from 'react';
 import { FallbackProps } from 'react-error-boundary';
-import { Translate } from './Translate';
+import { useTranslation } from 'react-i18next';
 
 export const ErrorFallbackPageProps: FC<FallbackProps> = (): JSX.Element => {
-  const { translate } = useLanguageContext();
+  const { t } = useTranslation();
 
   return (
     <Stack justifyContent="center" alignItems="center" spacing={2} maxWidth="100vw" maxHeight="100vh">
       <Card sx={{ maxWidth: 800 }}>
         <CardMedia component="img" height="400" image="/images/fallback.png" alt="Opps" />
         <CardHeader
-          title={translate('errors.generic.title')}
+          title={t('errors.generic.title')}
           titleTypographyProps={{ align: 'center', variant: 'h2', color: 'text.secondary' }}
         />
         <CardContent>
           <Stack spacing={2}>
             <Typography variant="body1" color="text.secondary">
-              <Translate textKey="errors.generic.message.part1" />
+              {t('errors.generic.message.part1')}
             </Typography>
 
             <Typography variant="body1" color="text.secondary">
-              <Translate textKey="errors.generic.message.part2" />
+              {t('errors.generic.message.part2')}
             </Typography>
 
             <Typography variant="body1" color="text.secondary">
-              <Translate textKey="errors.generic.message.part3" />{' '}
+              {t('errors.generic.message.part3')}{' '}
               <Link href="mailto:webmaster@max-janorschke.de">webmaster@max-janorschke.de</Link>
             </Typography>
 
             <Stack direction="row" spacing={2} justifyContent="center">
               <Button href={paths.root.pattern} variant="contained">
-                <Translate textKey="menu.home" />
+                {t('menu.home')}
               </Button>
             </Stack>
           </Stack>
